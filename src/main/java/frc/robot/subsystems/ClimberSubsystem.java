b815de18b65a16d629d9845a4deba87c27d6841e
AliceRacoo <assa8805238@gmail.com>
2026-03-04 17:27:08 +0200

    Update ClimberSubsystem.java
    הוספתי states
    ושיניתי לkMAXMotionPositionControl

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -165,7 +165,7 @@ public class ClimberSubsystem extends SubsystemBase {
 
   private void HandleL1() {
     if (!hasExtendedL1) {
-    setPositionGround(Constants.ClimberConstants.kL1ExtendHeight);\\2
+    setPositionGround(Constants.ClimberConstants.kL1ExtendHeight);
 
     if (state == ClimberState.AT_TARGET_GROUND) {
       hasExtendedL1 = true;
@@ -188,9 +188,18 @@ public class ClimberSubsystem extends SubsystemBase {
           setHeightCommandGround(0);
           break;
         case L1_EXTEND_TELEOP: // WORK HERE!!!!!!!!!!
-          HandleL1();
+          setPositionGround(Constants.ClimberConstants.kL1ExtendTeleop);
           break;
-        default:
+        case L1_CLOSE_TELEOP: 
+          setPositionHang(Constants.ClimberConstants.kL1CloseTeleop);
+          break;
+        case L1_EXTEND_AUTON:
+          setPositionGround(Constants.ClimberConstants.kL1ExtendAuton);
+          break;
+        case L1_CLOSE_AUTON:
+          setPositionHang(Constants.ClimberConstants.kL1CloseAuton);
+          break;
+          default:
           break;
       }
     }
@@ -198,18 +207,18 @@ public class ClimberSubsystem extends SubsystemBase {
 
   private void motorLogic() {
     if (isGrounded) {
-      closedLoop.setSetpoint(targetHeight, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+      closedLoop.setSetpoint(targetHeight, ControlType.kMAXMotionPositionControl, ClosedLoopSlot.kSlot0);
       if (Math.abs(targetHeight - getHeight()) < Constants.ClimberConstants.kTolerance) {
         state = ClimberState.AT_TARGET_GROUND;
       } else {
         state = ClimberState.MOVING_GROUND;
       }
     } else {
-      closedLoop.setSetpoint(targetHeight, ControlType.kPosition, ClosedLoopSlot.kSlot1);
+      closedLoop.setSetpoint(targetHeight, ControlType.kMAXMotionPositionControl, ClosedLoopSlot.kSlot1);
       if (Math.abs(targetHeight - getHeight()) < Constants.ClimberConstants.kTolerance) {
         state = ClimberState.AT_TARGET_HANG;
       } else {
-        state = ClimberState.MOVING_HANG; \\1
+        state = ClimberState.MOVING_HANG; 
       }
     }
   }
@@ -217,7 +226,7 @@ public class ClimberSubsystem extends SubsystemBase {
   public void setWantedState(WantedState wantedState) {
     this.currentWantedState = wantedState;
     if (wantedState != WantedState.L1_EXTEND_TELEOP) {
-    hasExtendedL1 = false;  \\3
+    hasExtendedL1 = false;
   }
   }
 }
